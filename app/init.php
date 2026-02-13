5ca62a9e993bf41edd4e4184bcb17f6b827dffae
allweDepth <115518499+allweDepth@users.noreply.github.com>
2026-02-13 22:27:01 +0800

    rev 2

--- app/init.php
+++ app/init.php
@@ -3,28 +3,33 @@
 
 define('BASE_PATH', dirname(__DIR__));
 
-// Load helpers (env function)
-require_once __DIR__ . '/helpers.php';
+// Load helpers jika ada
+if (file_exists(BASE_PATH . '/app/helpers.php')) {
+    require_once BASE_PATH . '/app/helpers.php';
+}
 
-// Autoload classes
+// Autoload classes (Controllers, Models, dll.)
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     $file = BASE_PATH . '/app/' . $class . '.php';
-    if (file_exists($file)) require $file;
+    if (file_exists($file)) {
+        require $file;
+    }
 });
 
 // Start session
 session_start();
 
-// View helper
-function view(string $path, array $data = []): void
-{
+// View helper (mendukung @extends & @section sederhana via ob)
+function view($path, $data = []) {
     extract($data);
     ob_start();
     require BASE_PATH . "/resources/views/{$path}.php";
     $content = ob_get_clean();
+
+    // Load layout utama
     require BASE_PATH . '/resources/views/layouts/app.php';
 }
 
-// Routes
+// Load routes
 require BASE_PATH . '/routes/web.php';
