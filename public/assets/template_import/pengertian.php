226281e178ca2bd82c98aa8b0f9df367fbd870b2
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-15 09:38:12 +0800

    upload template excel import

---
+++ public/assets/template_import/pengertian.php
@@ -0,0 +1,96 @@
+<table >
+	<tbody>
+		<tr>
+			<td data-label="Name">James</td>
+		</tr>
+		<tr>
+			<td data-label="Name">Jill</td>
+		</tr>
+		<tr>
+			<td data-label="Name">Elyse</td>
+		</tr>
+	</tbody>
+</table>
+<?php
+/*
+Pada Kodefikasi Sub Kegiatan, ditambahkan Metadata Sub Kegiatan. Metadata Sub Kegiatan adalah sekumpulan atribut informasi yang memberikan gambaran/dokumentasi dari penyelenggaraan sub kegiatan. Atribut informasi yang ditambahkan pada Metadata Sub Kegiatan terdiri atas kinerja, indikator dan satuan. Definisi dari kinerja, indikator dan satuan sebagai berikut:
+    a. Kinerja adalah hasil yang berupa keluaran (sub output) dari pelaksanaan sub kegiatan.
+    b. Indikator adalah alat ukur dari hasil pelaksanaan sub kegiatan.
+    c. Satuan adalah dasar ukuran dari indikator.
+
+Digit keempat merupakan urutan nomenklatur unit kerja/unit pelaksana teknis daerah/kelurahan yang telah ditetapkan dalam peraturan perundang-undangan mengenai perangkat daerah yang disajikan dengan kode |x.xx|x.xx|x.xx|xx.0001.
+Penyajian kodefikasi Organisasi atas urusan
+1.03.0.00.0.00.01.0000 DINAS PEKERJAAN UMUM DAN PENATAAN RUANG
+
+
+Penyajian kodefikasi Organisasi atas urusan pemerintahan dimaksud disajikan sebagai berikut:
+(1) Kode urusan pemerintahan wajib yang
+berkaitan dengan pelayanan dasar, meliputi:
+(a) pendidikan dengan kode 1-1;
+(b) kesehatan dengan kode 1-2;
+(c) pekerjaan umum dan penataan ruang
+dengan kode 1-3;
+(d) perumahan rakyat dan Kawasan
+permukiman dengan kode 1-4;
+(e) ketenteraman dan ketertiban umum serta
+perlindungan masyarakat dengan kode 1-
+5; dan
+(f) sosial dengan kode 1-6.
+(2) Kode urusan pemerintahan wajib yang tidak berkaitan dengan pelayanan dasar, meliputi:
+(a) tenaga kerja dengan kode 2-7;
+(b) pemberdayaan perempuan dan
+perlindungan anak dengan kode 2-8;
+(c) pangan dengan kode 2-9;
+(d) pertanahan dengan kode 2-10;
+(e) lingkungan hidup dengan kode 2-11;
+(f) administrasi kependudukan dan
+pencatatan sipil dengan kode 2-12;
+(g) pemberdayaan masyarakat dan desa
+dengan kode 2-13;
+- 22 -
+(h) pengendalian penduduk dan keluarga berencana dengan kode 2-14;
+(i) perhubungan dengan kode 2-15;
+(j) komunikasi dan informatika dengan kode
+2-16;
+(k) koperasi, usaha kecil dan menengah
+dengan kode 2-17;
+(l) penanaman modal dengan kode 2-18;
+(m) kepemudaan dan olahraga dengan kode 2-
+19;
+(n) statistik dengan kode 2-20;
+(o) persandian dengan kode 2-21;
+(p) kebudayaan dengan kode 2-22;
+(q) perpustakaan dengan kode 2-23; dan
+(r) kearsipan 2-24.
+(3) Kode urusan pemerintahan pilihan, meliputi:
+(a) kelautan dan perikanan dengan kode 3-
+25;
+(b) pariwisata dengan kode 3-26;
+(c) pertanian dengan kode 3-27;
+(d) kehutanan dengan kode 3-28;
+(e) energi dan sumber daya mineral dengan
+kode 3-29;
+(f) perdagangan dengan kode 3-30;
+(g) perindustrian dengan kode 3-31; dan
+(h) transmigrasi dengan kode 3-32.
+1. Kode Urusan/Unsur
+2. Kode
+
+
+
+objek belanja
+["gaji", "Belanja Gaji dan Tunjangan ASN"],
+									["barang_jasa_modal", "Belanja Barang Jasa dan Modal", "active"],
+									["bunga", "Belanja Bunga"],
+									["subsidi", "Belanja Subsidi"],
+									["hibah_barang_jasa", "Belanja Hibah (Barang/Jasa)"],
+									["hibah_uang", "Belanja Hibah (Uang)"],
+									["sosial_barang_jasa", "Belanja Bantuan Sosial (Barang/Jasa)"],
+									["sosial_uang", "Belanja Bantuan Sosial (Uang)"],
+									["keuangan_umum", "Belania Bantuan Keuangan Umum"],5.4.02
+									["keuangan_khusus", "Belanja Bantuan Keuangan Khusus"],
+									["btt", "Belanja Tidak Terduga (BTT)"],
+									["bos_pusat", "Dana BOS (BOS Pusat)"],
+									["blud", "Belanja Operasional (BLUD)"],
+									["lahan", "Pembebasan Tanah/ Lahan"]
+    */
