47d09b558266cddeff2fe2a022265c9d15e50b99
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-16 15:13:01 +0800

    ok

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,106 +1,123 @@
-<div class="main ui intro container">
-          <h2 class="ui dividing header">Pengantar untuk user </h2>
-          <div class="ui large info message">
-            <h2 class="ui header dash_header"><i class="settings icon"></i>
-              <div class="content">seSendok <div class="sub header">seSendok – Sistem Elektronik Sinkronisasi Dokumen Perencanaan, Penganggaran, dan Realisasi Kinerja merupakan aplikasi perencanaan, angaran dan
-                  realisasi Berbasis web</div>
-              </div>
-            </h2>
-          </div>
-          <div class="ui info message">
-            <h3 class="ui header dash_header"><i class="upload icon"></i>
-              <div class="content">menginpor file pada aplikasi <?php echo Auth::tahun(); ?>? <div class="sub header">
-                  <div class="ui divided selection list">
-                    <li class="item">file yang di Impor harus extension <a class="ui green label custom csv_format"><i class="file excel icon"></i>xlsx</a>,
-                      file template pengimporan dapat di download di <a class="ui teal tag label">menu data
-                        umum</a></li>
-                    <li class="item">Format angka menggunakan regional Indonesia <a class="ui blue label"><i class="money check icon"></i>pengelompokan " . "</a><a class="ui blue label"><i class="money check icon"></i>desimal " , "</a><a class="ui blue label"><i class="money check icon"></i>contoh "1.200.000,50"</a></li>
-                  </div>
+<style>
+.dashboard-wrapper{
+    min-height:100%;
+    display:flex;
+    flex-direction:column;
+}
+
+.dashboard-content{
+    flex:1;
+}
+</style>
+
+<div class="dashboard-wrapper">
+
+    <div class="dashboard-content">
+
+        <div class="main ui intro container">
+
+            <h2 class="ui dividing header">Pengantar untuk user </h2>
+
+            <div class="ui large info message">
+                <h2 class="ui header dash_header">
+                    <i class="settings icon"></i>
+                    <div class="content">
+                        seSendok
+                        <div class="sub header">
+                            seSendok – Sistem Elektronik Sinkronisasi Dokumen Perencanaan,
+                            Penganggaran, dan Realisasi Kinerja merupakan aplikasi
+                            perencanaan, anggaran dan realisasi berbasis web
+                        </div>
+                    </div>
+                </h2>
+            </div>
+
+            <div class="ui info message">
+                <h3 class="ui header dash_header">
+                    <i class="upload icon"></i>
+                    <div class="content">
+                        menginpor file pada aplikasi <?php echo Auth::tahun(); ?>?
+                        <div class="sub header">
+                            <div class="ui divided selection list">
+                                <div class="item">
+                                    file harus extension
+                                    <a class="ui green label">
+                                        <i class="file excel icon"></i>xlsx
+                                    </a>
+                                </div>
+                                <div class="item">
+                                    Format angka Indonesia:
+                                    <a class="ui blue label">1.200.000,50</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </h3>
+            </div>
+
+            <h2 class="ui dividing header">Cara menggunakan</h2>
+
+            <div class="ui relaxed divided list">
+                <div class="item">
+                    <i class="large list ol middle aligned icon"></i>
+                    <div class="content">
+                        <div class="header">Referensi >> Wilayah</div>
+                        <div class="description">Input kode wilayah (admin)</div>
+                    </div>
                 </div>
-              </div>
-            </h3>
-          </div>
-          <div class="ui icon buttons mini align">
-            <button class="ui button"><i class="align left icon"></i></button>
-            <button class="ui button"><i class="align center icon"></i></button>
-            <button class="ui button"><i class="align right icon"></i></button>
-            <button class="ui button"><i class="align justify icon"></i></button>
-          </div>
-          <div class="ui icon buttons mini font">
-            <button class="ui button"><i class="bold icon"></i></button>
-            <button class="ui button"><i class="underline icon"></i></button>
-            <button class="ui button"><i class="text width icon"></i></button>
-          </div>
-          <div class="ui dropdown">
-            <div class="text">Pilih Opsi</div>
-            <div class="menu">
-              <div class="item no-margin no-padding">
-                <div class="ui icon buttons mini">
-                  <button class="ui button"><i class="align left icon"></i></button>
-                  <button class="ui button"><i class="align center icon"></i></button>
-                  <button class="ui button"><i class="align right icon"></i></button>
-                  <button class="ui button"><i class="align justify icon"></i></button>
+
+                <div class="item">
+                    <i class="large list ol middle aligned icon"></i>
+                    <div class="content">
+                        <div class="header">Referensi >> Peraturan</div>
+                        <div class="description">Input peraturan (admin)</div>
+                    </div>
                 </div>
-                <div class="ui icon buttons mini">
-                  <button class="ui button"><i class="bold icon"></i></button>
-                  <button class="ui button"><i class="underline icon"></i></button>
-                  <button class="ui button"><i class="text width icon"></i></button>
+
+                <div class="item">
+                    <i class="large list ol middle aligned icon"></i>
+                    <div class="content">
+                        <div class="header">Pengaturan >> Tahun Anggaran</div>
+                        <div class="description">Tentukan Tahun Anggaran</div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <h2 class="ui dividing header">Cara menggunakan <a class="anchor"></a></h2>
-          <div class="ui relaxed divided list">
-            <div class="item">
-              <i class="large list ol middle aligned icon"></i>
-              <div class="content">
-                <a class="header">Referensi >> Wilayah</a>
-                <div class="description">input kode wilayah (admin)</div>
-              </div>
-            </div>
-            <div class="item">
-              <i class="large list ol middle aligned icon"></i>
-              <div class="content">
-                <a class="header">Referensi >> Peraturan</a>
-                <div class="description">input peraturan (admin)</div>
-              </div>
-            </div>
-            <div class="item">
-              <i class="large list ol middle aligned icon"></i>
-              <div class="content">
-                <a class="header">Pengaturan >> Tahun Anggaran</a>
-                <div class="description">tentukan Peraturan Tahun Anggaran (admin)</div>
-              </div>
-            </div>
-            <div class="item">
-              <i class="large list ol middle aligned icon"></i>
-              <div class="content">
-                <a class="header">Referensi >> Organisasi</a>
-                <div class="description">input Organisasi (admin)</div>
-              </div>
-            </div>
-            <div class="item">
-              <i class="large list ol middle aligned icon"></i>
-              <div class="content">
-                <a class="header">Referensi</a>
-                <div class="description">import/input referensi lainnya sampai sub kegiatan (admin)</div>
-              </div>
-            </div>
-          </div>
-          <p>Tutorial cara menggunakan aplikasi seSendok untuk penyusunan anggaran dapat di download <a href="template/tutorial_user.pdf" target="_blank">disini</a></p>
-          <p></p>
-          <p></p>
+
+            <p>
+                Tutorial dapat di download
+                <a href="template/tutorial_user.pdf" target="_blank">disini</a>
+            </p>
+
         </div>
-        <div class="ui vertical footer segment">
-          <div class="three column divided stackable center aligned ui grid">
+
+    </div>
+
+    <!-- FOOTER -->
+    <div class="ui vertical footer segment">
+        <div class="three column divided stackable center aligned ui grid">
+
             <div class="column">
-              <div class="ui icon header"><i class="teal rocket circular icon"></i>AHSP : <a href="javascript: void(0)">efisiensi dan efektif</a></div>
+                <div class="ui icon header">
+                    <i class="teal rocket circular icon"></i>
+                    AHSP : <a href="javascript:void(0)">efisiensi dan efektif</a>
+                </div>
             </div>
+
             <div class="column">
-              <div class="ui icon header"><i class="teal theme circular icon"></i>transparansi, <a href="javascript: void(0)">akuntabilitas</a></div>
+                <div class="ui icon header">
+                    <i class="teal theme circular icon"></i>
+                    transparansi, <a href="javascript:void(0)">akuntabilitas</a>
+                </div>
             </div>
+
             <div class="column">
-              <div class="ui icon header"><i class="teal food circular icon"></i>serta <a href="javascript: void(0)">partisipatif</a></div>
+                <div class="ui icon header">
+                    <i class="teal food circular icon"></i>
+                    serta <a href="javascript:void(0)">partisipatif</a>
+                </div>
             </div>
-          </div>
+
         </div>
+    </div>
+
+</div>
