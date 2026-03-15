b2b5b36ef55c41eb78deeb93d613506f492fcd00
allweDepth <dpupr.pasangkayu@gmail.com>
2026-03-15 14:45:26 +0800

    rev

--- public/assets/js/modules/wallchat.js
+++ public/assets/js/modules/wallchat.js
@@ -14,50 +14,69 @@ class WallchatModule {
 	}
 
 	init() {
-		this.renderLayout();
 		this.bindEvents();
+		this.initDropdown();
 	}
 
-	renderLayout() {
-		if ($("#chat-box").length) return;
-		const html = `
-            <div class="ui segment">
-                <h3 class="ui header">Wallchat</h3>
-                <div id="chat-box"></div>
-                <form id="chat-form" class="ui form">
-                    <div class="field">
-                        <input type="text" name="message" placeholder="Ketik pesan...">
-                    </div>
-                    <button class="ui primary button">Kirim</button>
-                </form>
-            </div>
-        `;
+	// renderLayout() {
+	// 	if ($("#chat-box").length) return;
+	// 	const html = `
+	//           <div class="ui segment">
+	//               <h3 class="ui header">Wallchat</h3>
+	//               <div id="chat-box"></div>
+	//               <form id="chat-form" class="ui form">
+	//                   <div class="field">
+	//                       <input type="text" name="message" placeholder="Ketik pesan...">
+	//                   </div>
+	//                   <button class="ui primary button">Kirim</button>
+	//               </form>
+	//           </div>
+	//       `;
 
-		if ($("#chat-box").length) return;
+	// 	if ($("#chat-box").length) return;
 
-		$(this.mainContainer).append(html);
-	}
+	// 	$(this.mainContainer).append(html);
+	// }
 
 	bindEvents() {
-		$(document).on("submit", "#chat-form", (e) => {
+		$(document).off("submit", "#formPost");
+
+		$(document).on("submit", "#formPost", (e) => {
 			e.preventDefault();
 
-			const message = $('input[name="message"]').val();
+			const content = $('#formPost textarea[name="content"]').val();
 
 			this.ajax.request({
 				data: {
-					action: "insert",
+					action: "add",
 					tbl: "wallchat",
-					message: message,
+					content: content,
 				},
 
-				success: () => {
-					$("#chat-form")[0].reset();
+				success: (res) => {
+					$("#formPost textarea").val("");
+
+					this.reloadFeed();
 				},
 			});
 		});
 	}
+	reloadFeed() {
+		fetch("/wallchat/feed", {
+			headers: { "X-Requested-With": "XMLHttpRequest" },
+		})
+			.then((res) => res.text())
+			.then((html) => {
+				$("#feedContainer").html(html);
+			});
+	}
+	initDropdown() {
+		$(".ui.dropdown").dropdown();
 
+		$("#btnPrivateMessage").on("click", () => {
+			$("#modalPrivateMessage").modal("show");
+		});
+	}
 	destroy() {
 		$(document).off("submit", "#chat-form");
 
