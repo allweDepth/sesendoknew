17acd714c35ff15075b973e36c0809f7b32c0a5f
allweDepth <dpupr.pasangkayu@gmail.com>
2026-03-23 00:43:25 +0800

    revisi

--- public/assets/js/modules/kontrak.js
+++ public/assets/js/modules/kontrak.js
@@ -1,11 +1,25 @@
 class KontrakModule extends BaseCrudModule {
 	// // WAJIB extend BaseCrudModule
-
 	constructor() {
 		super({
 			moduleName: "kontrak",
 			menuItems: [],
 			useMenu: false,
+
+			// // tbl default agar bisa dipakai internal
+			defaultTable: {
+				tbl: "kontrak",
+				req: "kontrak_neo",
+			},
 		});
+	}
+
+	init() {
+		super.init();
+
+		// // trigger manual tanpa menu & tanpa URL
+		if (!this.currentTable) {
+			this.loadTable("kontrak", "kontrak");
+		}
 	}
 }
