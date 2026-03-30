0a3821c46e10af860524b4083134c3f1751d2747
allweDepth <dpupr.pasangkayu@gmail.com>
2026-03-30 23:31:40 +0800

    revisi mau tambah menu tata naskah dokumen

--- app/Views/tata_naskah/dashboard.php
+++
@@ -1,8 +0,0 @@
-<div class="ui centered card">
-    <div class="content">
-        <a class="header">Halo</a>
-        <div class="description">
-            Belum  Hahahahahah
-        </div>
-    </div>
-</div>
